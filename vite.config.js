2723d8be7db6f2abb84c3f8710b2f797b50b167b
abdulak26rehman-commits <abdula.k.26rehman@gmail.com>
2026-04-15 12:13:01 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/uet-erp-pro-frontend/',
   plugins: [react()],
-  server: { port: 5173 },
-  base: './',
+  server: {
+    port: 5173
+  }
 })
